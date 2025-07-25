bddb0f92f55d5b27df5595f09c184dbeeddab173
RonaldMullo <electroascensores64@gmail.com>
2025-07-25 01:25:31 -0500

    W02 again

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Obtener el año actual para el copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Obtener la última fecha de modificación
+document.getElementById('lastModified').textContent = "Last modification: " + document.lastModified;
