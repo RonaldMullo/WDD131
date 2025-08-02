9ae8e2e8805f82a81b37f6be5c650ce51511b022
RonaldMullo <electroascensores64@gmail.com>
2025-08-02 16:22:32 -0500

    Act

--- styles/scripts/place.js
+++ styles/scripts/place.js
@@ -1,8 +1,8 @@
-// Actualizar año y última modificación
+//Update year
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('last-modified').textContent = document.lastModified;
 
-// Cálculo de Wind Chill (en °C)
+//  Wind Chill Calculation ( °C)
 function calculateWindChill(temp, windSpeed) {
     if (temp <= 10 && windSpeed > 4.8) {
         const windChill = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temp * Math.pow(windSpeed, 0.16));
@@ -11,7 +11,7 @@ function calculateWindChill(temp, windSpeed) {
     return "N/A";
 }
 
-// Valores estáticos (ejemplo)
+// Static values (example)
 const temperature = 10;
 const windSpeed = 5;
 
